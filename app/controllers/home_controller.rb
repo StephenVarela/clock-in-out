47236ebeb0c68dcfe0929b9bab01b80ba80bee1b
StephenVarela <varelast92@gmail.com>
2020-02-15 14:41:06 -0500

    Redirect to home page and add user login dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   def index
+    if current_user.present?
+      time_card = current_user.time_cards&.last
+
+      if time_card.nil?
+        @time_card = TimeCard.new
+      elsif time_card.time_out.nil?
+        @time_card = time_card
+      else
+        @time_card = TimeCard.new
+      end
+    end 
   end
 end
