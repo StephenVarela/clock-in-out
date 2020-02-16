ab22a04529c53aa6bd0bc5a54ddaffced83c881a
StephenVarela <varelast92@gmail.com>
2020-02-15 19:16:46 -0500

    Add total time functionality

--- app/models/time_card.rb
+++ app/models/time_card.rb
@@ -1,3 +1,10 @@
 class TimeCard < ApplicationRecord
   belongs_to :user
+  before_save :calculate_total_time
+
+  def calculate_total_time
+    return if time_out.nil?
+
+    self.total_time = time_out - time_in
+  end
 end
