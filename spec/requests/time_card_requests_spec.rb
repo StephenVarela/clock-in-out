1c886db80b8b60ea15f03bde4e240cac324bf6c8
StephenVarela <varelast92@gmail.com>
2020-02-16 13:18:25 -0500

    Add users and time_cards requests specs

---
+++ spec/requests/time_card_requests_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+require 'byebug'
+
+RSpec.describe TimeCardsController, type: :request do           
+  describe 'GET /index' do
+    let!(:user) { FactoryBot.create(:user) }
+    let!(:time_cards) { FactoryBot.create_list(:time_card, 10, user: user) }
+    
+    context 'logged out' do
+      it 'redirects to home' do 
+        get time_cards_path
+        expect(response).to have_http_status(302)
+      end
+    end
+    
+    context 'logged in' do
+      before do 
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      end
+      
+      it 'responds with status 200' do 
+        get time_cards_path
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+end
