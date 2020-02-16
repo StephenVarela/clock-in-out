2fe2a7721aec2fb931e5fc52a5f2e91c70710d49
StephenVarela <varelast92@gmail.com>
2020-02-16 00:02:15 -0500

    Add validation for time_in greater than time_out

--- app/models/time_card.rb
+++ app/models/time_card.rb
@@ -1,6 +1,7 @@
 class TimeCard < ApplicationRecord
   belongs_to :user
   before_save :calculate_total_time
+  validate :clock_in_less_than_clock_out 
 
   def time_in_hours
     total_time.nil? ? 0 : total_time / 60.0 / 60.0  
@@ -10,5 +11,13 @@ class TimeCard < ApplicationRecord
     return if time_out.nil?
 
     self.total_time = time_out - time_in
+  end
+
+  def clock_in_less_than_clock_out
+    if time_out.present?
+      if time_in.to_i > time_out.to_i
+        errors.add(:time_out, "can't be less than time in")
+      end
+    end
   end
 end
--- spec/models/time_card_spec.rb
+++ spec/models/time_card_spec.rb
@@ -3,6 +3,13 @@ require 'byebug'
 
 RSpec.describe :time_card, type: :model do           
   describe '.valid?' do
-
+    context 'time out is before time in' do 
+      let(:user) { FactoryBot.create(:user) }
+      let(:time_card) { FactoryBot.build(:time_card, user: user, time_in: Time.now, time_out: 5.minutes.ago) }
+      
+      it 'is not valid' do 
+        expect(time_card).to be_invalid
+      end
+    end
   end
 end
