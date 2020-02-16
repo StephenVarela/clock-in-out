1f3ea14d0391f40251be65a274680957c545703d
StephenVarela <varelast92@gmail.com>
2020-02-15 23:27:03 -0500

    Fix up button styling and navigation

--- app/models/time_card.rb
+++ app/models/time_card.rb
@@ -2,6 +2,10 @@ class TimeCard < ApplicationRecord
   belongs_to :user
   before_save :calculate_total_time
 
+  def time_in_hours
+    total_time.nil? ? 0 : total_time / 60.0 / 60.0  
+  end
+  
   def calculate_total_time
     return if time_out.nil?
 
